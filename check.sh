36c5e9bf72ab18dab400711701cdc001b8199cf7
Karl Fogel <kfogel@red-bean.com>
2008-07-15 02:23:56 +0000

    * check.sh: Put option-parsing tests before functionality tests.

--- check.sh
+++ check.sh
@@ -11,6 +11,55 @@ cp -a dot-onetime test-tmp
 rm -rf test-tmp/dot-onetime/.svn
 cd test-tmp
 
+############################################################################
+###  Option-parsing tests.                                               ###
+############################################################################
+
+###
+# In the tests of the various option parsing methods, "e.N" is
+# encrypted text and "d.N" is decrypted text.
+###
+
+# mode 1
+../../onetime --config=dot-onetime -e -p ../random-data-2 -o e.1 ../test-msg
+../../onetime --config=dot-onetime -d -p ../random-data-2 -o d.1 e.1
+
+# mode 2
+../../onetime --config=dot-onetime -e -p ../random-data-2 ../test-msg
+mv ../test-msg.onetime e.2.onetime
+../../onetime --config=dot-onetime -d -p ../random-data-2 e.2.onetime
+mv e.2 d.2
+
+# mode 3
+../../onetime --config=dot-onetime -e -p ../random-data-2 -o - ../test-msg > e.3
+../../onetime --config=dot-onetime -d -p ../random-data-2 -o - e.3 > d.3
+
+# mode 4
+../../onetime --config=dot-onetime -e -p ../random-data-2 < ../test-msg > e.4
+../../onetime --config=dot-onetime -d -p ../random-data-2 < e.4 > d.4
+
+# mode 5
+../../onetime --config=dot-onetime -e -p ../random-data-2 -o e.5 < ../test-msg
+../../onetime --config=dot-onetime -d -p ../random-data-2 -o d.5 < e.5
+
+PASSED="yes"
+for n in 1 2 3 4 5; do
+  if cmp ../test-msg d.${n}; then
+    true
+  else
+    echo "Error: option-parsing tests failed, something went wrong."
+    PASSED="no"
+  fi
+done
+
+if [ ${PASSED} = "yes" ]; then
+  echo "Option-parsing tests passed."
+fi
+
+############################################################################
+###  Functionality tests.                                                ###
+############################################################################
+
 # Print the (string) first argument, then display all pad lengths.
 # NOTE: Deactivated by default.  Change 'false' to 'true' to turn on.
 function show_lengths()
@@ -54,48 +103,7 @@ show_lengths "After encoding again:"
 show_lengths "After decoding:"
 
 if cmp ../test-msg test-msg.decoded-1; then
-  echo "Basic tests passed."
+  echo "Functionality tests passed."
 else
-  echo "Error: basic tests failed, something went wrong."
-fi
-
-###
-# Test the various option parsing methods.
-# "e.N" is encrypted text, "d.N" is decrypted text.
-###
-
-# mode 1
-../../onetime --config=dot-onetime -e -p ../random-data-2 -o e.1 ../test-msg
-../../onetime --config=dot-onetime -d -p ../random-data-2 -o d.1 e.1
-
-# mode 2
-../../onetime --config=dot-onetime -e -p ../random-data-2 ../test-msg
-mv ../test-msg.onetime e.2.onetime
-../../onetime --config=dot-onetime -d -p ../random-data-2 e.2.onetime
-mv e.2 d.2
-
-# mode 3
-../../onetime --config=dot-onetime -e -p ../random-data-2 -o - ../test-msg > e.3
-../../onetime --config=dot-onetime -d -p ../random-data-2 -o - e.3 > d.3
-
-# mode 4
-../../onetime --config=dot-onetime -e -p ../random-data-2 < ../test-msg > e.4
-../../onetime --config=dot-onetime -d -p ../random-data-2 < e.4 > d.4
-
-# mode 5
-../../onetime --config=dot-onetime -e -p ../random-data-2 -o e.5 < ../test-msg
-../../onetime --config=dot-onetime -d -p ../random-data-2 -o d.5 < e.5
-
-PASSED="yes"
-for n in 1 2 3 4 5; do
-  if cmp ../test-msg d.${n}; then
-    true
-  else
-    echo "Error: option parsing tests failed, something went wrong."
-    PASSED="no"
-  fi
-done
-
-if [ ${PASSED} = "yes" ]; then
-  echo "Option parsing tests passed."
+  echo "Error: functionality tests failed, something went wrong."
 fi
