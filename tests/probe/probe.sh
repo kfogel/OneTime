1733cbf1675b74503b0acbd5231d5f586cd39325
Karl Fogel <kfogel@red-bean.com>
2016-06-16 22:54:39 -0500

    Expand probe test to try all possible byte values

--- tests/probe/probe.sh
+++ tests/probe/probe.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
+# Permute every byte of a OneTime encrypted message through all
+# possible values, and see what results we get.  At least some of
+# those results should be message digest integrity errors.
+#
+# Suggested usage: 'probe.sh > probe.out 2>&1'
+
 for posn in `python -c "for i in range(195,1050): print i"`; do
-  cp input.onetime try-${posn}.onetime
-  ../zap try-${posn}.onetime ${posn} ? 65
-  echo "Trying posn ${posn}:"
-  ../../onetime -n -d -p ../test-pad-1 try-${posn}.onetime
+  cp input.onetime this-try.onetime
+  for newval in `python -c "for i in range(0,255): print i"`; do
+    ../zap try-${posn}.onetime ${posn} ? ${newval}
+    echo ""
+    echo "####################################################"
+    echo "Trying ${newval} at position ${posn}:"
+    # Actually, on one iteration ${newval} will be the same as
+    # whatever value is already at ${posn}, but that just means the
+    # message will decrypt perfectly, which is fine.
+    ../../onetime -n -d -p ../test-pad-1 this-try.onetime
+  done
 done
