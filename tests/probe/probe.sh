70f7c38ab6524ac73338c9c1a51005b18ed8ce90
Karl Fogel <kfogel@red-bean.com>
2016-06-19 15:21:53 -0500

    Fix loop refresh bug, and print when success

--- tests/probe/probe.sh
+++ tests/probe/probe.sh
@@ -7,8 +7,9 @@
 # Suggested usage: './probe.sh > probe.out 2>&1'
 
 for posn in `python -c "for i in range(195,1046): print i"`; do
-  cp input.onetime this-try.onetime
   for newval in `python -c "for i in range(0,256): print i"`; do
+    rm this-try
+    cp input.onetime this-try.onetime
     ../zap this-try.onetime ${posn} ? ${newval}
     echo ""
     echo "####################################################"
@@ -17,5 +18,8 @@ for posn in `python -c "for i in range(195,1046): print i"`; do
     # whatever value is already at ${posn}, but that just means the
     # message will decrypt perfectly, which is fine.
     ../../onetime -n -d -p ../test-pad-1 this-try.onetime
+    if [ -f this-try ]; then
+      echo "Successful decryption."
+    fi
   done
 done
